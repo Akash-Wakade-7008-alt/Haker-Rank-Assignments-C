d2bad064cf4f38d45b5fec4ce018ea0b1f76854a
Akash-Wakade-7008-alt <akashwakade7008@gmail.com>
2025-12-06 19:20:30 +0530

    Update Sum of Two Numbers.c
    Adds sample input and output

--- Sum of Two Numbers.c
+++ Sum of Two Numbers.c
@@ -20,7 +20,24 @@ int main() {
     int sum=num_1+num_2;
     printf("The sum of %d and %d is %d.",num_1,num_2,sum);
     
-    
-    
     return 0;
 }
+
+// Sample Input 0
+
+// 3 5
+// Sample Output 0
+
+// The sum of 3 and 5 is 8.
+// Sample Input 1
+
+// 10 20
+// Sample Output 1
+
+// The sum of 10 and 20 is 30.
+// Sample Input 2
+
+// 7 2
+// Sample Output 2
+
+// The sum of 7 and 2 is 9.
