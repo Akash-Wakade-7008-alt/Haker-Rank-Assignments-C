d20d88cea7c81edd4f21772cfa344a90f8b27584
Akash-Wakade-7008-alt <akashwakade7008@gmail.com>
2025-12-06 19:17:01 +0530

    Update Half the number.c
    Adds Sample input and output

--- Half the number.c
+++ Half the number.c
@@ -18,3 +18,23 @@ int main() {
     printf("Half of %d is %.2f",num,res);  //%.2f->denotes that there will be only two numbers after decimal
     return 0;
 }
+
+
+// Sample Input 0
+
+// 10
+// Sample Output 0
+
+// Half of 10 is 5.00
+// Sample Input 1
+
+// 7
+// Sample Output 1
+
+// Half of 7 is 3.50
+// Sample Input 2
+
+// -8
+// Sample Output 2
+
+// Half of -8 is -4.00
